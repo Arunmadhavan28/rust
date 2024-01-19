0a55056a345683921a565db656363411f26839bc
Arunmadhavan28 <arunmadhavan2821@gmail.com>
2024-01-19 17:18:45 +0530

    the sample welcome

---
+++ welcome/target/debug/deps/welcome-1fe01232c92143b1.d
@@ -0,0 +1,5 @@
+/Users/arunmadhavanevr/dev/welcome/target/debug/deps/welcome-1fe01232c92143b1: src/main.rs
+
+/Users/arunmadhavanevr/dev/welcome/target/debug/deps/welcome-1fe01232c92143b1.d: src/main.rs
+
+src/main.rs:
---
+++ welcome/target/debug/welcome.d
@@ -0,0 +1,1 @@
+/Users/arunmadhavanevr/dev/welcome/target/debug/welcome: /Users/arunmadhavanevr/dev/welcome/src/main.rs
